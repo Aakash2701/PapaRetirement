f9db33780bca6231eb790b5da105b2343c35a6da
Aakash Aggarwal <aakash.aggarwal5@gmail.com>
2025-12-17 04:28:16 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         return (
           <GuestBook 
             onBack={() => setView('home')} 
-            onSubmitSuccess={() => setView('success')} 
+            onSubmit={() => setView('success')} 
           />
         );
       case 'success':
